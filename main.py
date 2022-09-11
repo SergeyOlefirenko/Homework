19e45bed9a2a0460574418a53b5cf68dbdc66d15
Sergey <olefirenkosergey@gmail.com>
2022-09-11 15:35:22 +0300

    Homework_18_Olefirenko

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Дописать приложение, Необходимо добавить возможность хранения в одном списке/файле
-# студентов и прподавателей.
-# Обратить внимание на методы чтения/записи в файл.
 
 #Создаем класс Person
 
@@ -25,6 +22,9 @@ class Person:
     def get_phone(self):
         return self.__phone
 
+    def get_title(self):
+        return self.__title
+
     def set_firstname(self, firstname: str):
         self.__firstname = firstname.capitalize()
 
@@ -43,24 +43,29 @@ class Person:
     def __str__(self):
         return f'{self.__firstname} {self.__lastname} {self.__phone} {self.__title}'
 
-#Создаем метод Save и сохраняем в файл объект класса person
+#Создаем метод Save для сохранение и перезаписи объектов класса person
+#Сoздаем метод Add для добавления записи о студентах и учителях в файл
 
+#Метод позволяет записать в файл объект класса
     def save(self):
         import sys
         person_stdout = sys.stdout
         with open('people.txt', mode='a', encoding='utf-8') as file:
             sys.stdout = file
             print(teacher)
-        with open('people.txt', mode='a', encoding='utf-8') as file:
-            sys.stdout = file
             print(student)
             sys.stdout = person_stdout
+
+    def add(self):
+        with open('people.txt', mode='a', encoding='utf-8') as file:
+            file.write(self.__str__() + '\n')
 
     def load(self):
         reader = open('people.txt', mode='r', encoding='UTF-8')
         text = reader.read()
         print(text)
         reader.close()
+
 person = Person('Имя', 'Фамилия','Номер телефона', 'Должность')
 
 #Создаем 2 класса Student и Teacher
@@ -78,24 +83,10 @@ class Student(Person):
     def set_group(self, group: str):
         self.__group = group
 
-    def to_file(self, filename: str):
-        with open(filename, mode='a', encoding='utf-8') as file:
-            file.write(self.__str__() + '\n')
-
-    def from_file(self, filename: str):
-        with open(filename, mode='r', encoding='utf-8') as file:
-            res = file.readline().split()
-            self.set_firstname(res[0])
-            self.set_lastname(res[1])
-            self.set_phone(res[2])
-            self.set_title(res[3])
-            self.set_group(res[4])
-
     def __str__(self):
         return f'{super().__str__()} {self.__group}'
 
-
-student = Student('Михаил', 'Самохвалов', '+380501232323,', 'Студент -', 'ПГС-21')
+student = Student('Михаил', 'Самохвалов', '+380501232323,', 'Студент,', 'ПГС-21')
 
 class Teacher(Person):
     __subject = str()
@@ -110,53 +101,28 @@ class Teacher(Person):
     def set_subject(self, subject: str):
         self.__subject = subject
 
-    def to_file(self, filename: str):
-        with open(filename, mode='a', encoding='utf-8') as file:
-            file.write(self.__str__() + '\n')
-
-    def from_file(self, filename: str):
-        with open(filename, mode='r', encoding='utf-8') as file:
-            res = file.readline().split()
-            self.set_firstname(res[0])
-            self.set_lastname(res[1])
-            self.set_phone(res[2])
-            self.set_title(res[3])
-            self.set_subject(res[4])
-
     def __str__(self):
         return f'{super().__str__()} {self.__subject}'
 
-teacher = Teacher('Василий', 'Пупкин', '+380501111111,','Преподаватель -','Химия')
+teacher = Teacher('Василий', 'Пупкин', '+380501111111,','Преподаватель,','Химия')
+
+
 person.save()
-# person.load()
+
+#Добавляем весь список студентов в файл при такой необходимости
 
 studentList = []
-studentList.append('Ivasyk Bulkin trinolyatrulyalya Студент Python11')
-studentList.append('Grigoiy Terkin +387415874165 Студент Python21')
-studentList.append('Anna Chechetkina +04478451235 Студент C++14')
-studentList.append('Svetlana Bulkina trinolyatrulyalya2 Студент Python11')
-studentList.append('Anatloiy Fedorov 0991234756 Студент C++17')
-def add():
-    import sys
-    person_stdout = sys.stdout
-    with open('people.txt', mode='a', encoding='utf-8') as file:
-        sys.stdout = file
-        for i in studentList:
-            print(i)
-        sys.stdout = person_stdout
-add()
-# person.load()
 
-studList = []
+studentList.append(Student('Вася', 'Котов', '+380501234567', 'Student', 'Python11'))
+studentList.append(Student('Коля', 'Николаев', '+387415874165', 'Student', 'Python21'))
+studentList.append(Student('Анна', 'Чечушкова', '+44784512358', 'Student', 'C++14'))
+studentList.append(Student('Светлана', 'Лобода', '+380671111111','Student', 'Python11'))
+studentList.append(Student('Михаил', 'Федоров', '+99123475611', 'Student','C++17'))
 
-studList.append(Student('Вася', 'Котов', '+380501234567', 'Student', 'Python11'))
-studList.append(Student('Коля', 'Николаев', '+387415874165', 'Student', 'Python21'))
-studList.append(Student('Анна', 'Чечушкова', '+44784512358', 'Student', 'C++14'))
-studList.append(Student('Светлана', 'Лобода', '+380671111111','Student', 'Python11'))
-studList.append(Student('Михаил', 'Федоров', '+99123475611', 'Student','C++17'))
+for i in studentList:
+    i.add()
 
-for i in studList:
-    i.to_file('people.txt')
+#Добавляем весь список учителей в файл при такой необходимости
 
 teacherList = []
 
@@ -167,31 +133,86 @@ teacherList.append(Teacher('Инна', 'Мороз', '+380671234567', 'Teacher',
 teacherList.append(Teacher('Алексей', 'Семихвостов', '+99123475611', 'Teacher', 'C++17'))
 
 for i in teacherList:
-    i.to_file('people.txt')
+    i.add()
 person.load()
-#     print(i)
-# studList[0].to_file('people.txt') # Не работает. Если вводить studList[0][1][2][3][4]-выдает ошибку
-# studList[0].from_file('people.txt')# Не работает. Если вводить studList[0][1][2][3][4]-выдает ошибку
-student.from_file('people.txt') #Не работает либо работает но я не понимаю как
-teacher.from_file('people.txt') #Не работает либо работает но я не понимаю как
+#Добавляем студентов и учителей в файл по отдельности
 
-# def decorator(funk):
-#     def add():
-#         import sys
-#         person_stdout = sys.stdout
-#         with open('people.txt', mode='a', encoding='utf-8') as file:
-#             sys.stdout = file
-#             for i in studentList:
-#                 print(i)
-#             sys.stdout = person_stdout
-#         funk()
-#     return add()
-#
-# def save():
-#     person.save()
-#     # person.load()
-# dec_funktion = decorator(save)
+studentList[0].add()
+studentList[1].add()
+studentList[2].add()
+studentList[3].add()
+studentList[4].add()
+teacherList[0].add()
+teacherList[1].add()
+teacherList[2].add()
+teacherList[3].add()
+teacherList[4].add()
 
+import json
 
+studentGroup = {
+    'Name': student.get_firstname(),
+    'Lastname': student.get_lastname(),
+    'Phone number': student.get_phone(),
+    'Title': student.get_title(),
+    'Group': student.get_group()
+}
+teacherGroup = {
+'Name': teacher.get_firstname(),
+    'Lastname': teacher.get_lastname(),
+    'Phone number': teacher.get_phone(),
+    'Title': teacher.get_title(),
+    'Subject': teacher.get_subject()
+}
+Group ={
+    'Students': studentGroup,
+    'Teachers': teacherGroup
+}
+
+with open('serialize.txt', mode='w', encoding='UTF-8') as file:
+     json.dump(Group, file, indent = 4)
+
+with open('serialize.txt', mode='r', encoding='UTF-8') as file:
+    deserialize = json.load(file)
+    print(deserialize)
+
+p = Person
+#Данное действие позволяет записать данные из списка в файл, создав, при этом словарь, а также дописать данные
+for p in studentList:
+    import sys
+    person_stdout = sys.stdout
+    with open('serialize.txt', mode='a', encoding='utf-8') as file:
+        json_string = json.dumps(p.__dict__, indent=4)
+        sys.stdout = file
+        print(json_string)
+        sys.stdout = person_stdout
+#Строки десериализирует если раскомментировать но получить декодированные данные из самого файла не получается
+    Person = json.loads(json_string)
+    print(Person)
+
+#Данное действие позволяет записать данные из списка в файл, создав, при этом словарь, а также дописать данные
+for p in teacherList:
+    import sys
+    person_stdout = sys.stdout
+    with open('serialize.txt', mode='a', encoding='utf-8') as file:
+        json_string = json.dumps(p.__dict__, indent=4)
+        sys.stdout = file
+        print(json_string)
+        sys.stdout = person_stdout
+# Строки десериализирует если раскомментировать но получить декодированные данные из самого файла не получается
+    Person = json.loads(json_string)
+    print(Person)
+
+# Не получается десериализировать данные из файла, (load) тоже не работает
+# Пока нет понимания как декодировать TextIOWrapper в строку
+# with open('serialize.txt', mode='r', encoding='UTF-8') as file:
+#     deserialize = json.loads(file)
+#     print(deserialize)
+
+# Если раскомментировать - считывает строки из файла в одну строку без декодирования...
+# import json
+# with open('serialize.txt', 'r') as f:
+#     json_str = json.dumps(f.readlines())
+# print(json_str)
 
 
